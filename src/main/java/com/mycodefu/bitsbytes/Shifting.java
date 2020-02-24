cb97e3a717437e78d9ee60eb0c38b78acc7aaf66
Luke Thompson <endeavour9@gmail.com>
2020-02-24 18:45:34 +1100

    fix the log message pluralisation

--- src/main/java/com/mycodefu/bitsbytes/Shifting.java
+++ src/main/java/com/mycodefu/bitsbytes/Shifting.java
@@ -4,11 +4,13 @@ public class Shifting {
     public static void main(String[] args) {
         int shifty = 255;
         for (int i = 0; i < 32; i++) {
-            System.out.printf("int value %d shifted %d bit%s left: %d\n", shifty, i, i == 1 ? "s" : "", shifty << i);
+            int shifted = shifty << i;
+            System.out.printf("int value %d shifted %d bit%s left: %d\n", shifty, i, i == 1 ? "" : "s", shifted);
         }
 
         for (int i = 0; i < 32; i++) {
-            System.out.printf("int value %d shifted %d bit%s right: %d\n", shifty, i, i == 1 ? "s" : "", shifty >> i);
+            int shifted = shifty >> i;
+            System.out.printf("int value %d shifted %d bit%s right: %d\n", shifty, i, i == 1 ? "" : "s", shifted);
         }
 
     }
