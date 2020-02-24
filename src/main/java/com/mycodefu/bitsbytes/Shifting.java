abf6130550f453e423a57914a3ea5b656d04d7f8
Luke Thompson <endeavour9@gmail.com>
2020-02-24 19:04:56 +1100

    add binary representations of the numbers

--- src/main/java/com/mycodefu/bitsbytes/Shifting.java
+++ src/main/java/com/mycodefu/bitsbytes/Shifting.java
@@ -6,18 +6,18 @@ public class Shifting {
     public static void main(String[] args) {
         int shifty = 255;
         String shiftyBits = StringUtils.leftPad(Integer.toBinaryString(shifty), 32, "0");
+        System.out.printf("int value %d (%s)\n", shifty, shiftyBits);
 
         for (int i = 0; i < 32; i++) {
             int shifted = shifty << i;
             String shiftedBits = StringUtils.leftPad(Integer.toBinaryString(shifted), 32, "0");
-            System.out.printf("int value %d (%s) shifted %d bit%s left: %d (%s)\n", shifty, shiftyBits, i, i == 1 ? "" : "s", shifted, shiftedBits);
+            System.out.printf("shifted %d bit%s left: %d (%s)\n", i, i == 1 ? "" : "s", shifted, shiftedBits);
         }
 
         for (int i = 0; i < 32; i++) {
             int shifted = shifty >> i;
             String shiftedBits = StringUtils.leftPad(Integer.toBinaryString(shifted), 32, "0");
-            System.out.printf("int value %d (%s) shifted %d bit%s right: %d (%s)\n", shifty, shiftyBits, i, i == 1 ? "" : "s", shifted, shiftedBits);
-
+            System.out.printf("shifted %d bit%s right: %d (%s)\n", i, i == 1 ? "" : "s", shifted, shiftedBits);
         }
 
     }
